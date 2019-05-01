bb3187efc2e1dca756f1cd67015ddf052f72ca1e
rseligm2 <rseligm2@illinois.edu>
2019-05-01 15:03:00 -0500

    Roatis is a loser

--- SecurityWikiScraper/venv/FormatFile.py
+++ SecurityWikiScraper/venv/FormatFile.py
@@ -1,13 +1,22 @@
 import re
 import os
 
+
+#remove braces, changes dashes to underscores
 path = 'HTML_Files'
-sitelist = open('sitelist', 'a')
+sitelist = open('index.js', 'a')
+sitelist2 = open('sitelist.txt', 'a')
 for filename in os.listdir(path):
     variname = re.sub('.html', '', filename)
-    sitelist.write(variname + '\n')
+    variname = re.sub('\(|\)', '', variname)
+    variname = re.sub('-', '_', variname)
+    variname = re.sub('\.', '', variname)
+    sitelist.write('export {' + variname + '} from \'./'+ variname + '.js\';\n')
+    sitelist2.write('\'' + variname + '\', ')
     fileread = open('HTML_Files/' + filename, "r")
     content = fileread.read()
+    content = re.sub(r'\\', r'\\\\', content)
+    content = re.sub('`', r'\\`', content)
     content = re.sub('<!DOCTYPE html>', 'export const ' + variname + '= ` <!DOCTYPE html>', content)
     content = re.sub('</html>', '</html> `;', content)
     fileread.close()
